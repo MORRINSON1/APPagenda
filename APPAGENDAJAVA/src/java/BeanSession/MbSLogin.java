6d252215157baa5d7203c56b38f612a1ed9409b6
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2014-05-06 19:04:17 -0500

    INICIO Y CIERRE DE SESIÓN

---
+++ APPAGENDAJAVA/src/java/BeanSession/MbSLogin.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BeanSession;
+
+import Clases.Encrypt;
+import Dao.DaoTUsuario;
+import HibernateUtil.HibernateUtil;
+import Pojo.Tusuario;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author KevinArnold
+ */
+@Named(value = "mbSLogin")
+@SessionScoped
+public class MbSLogin implements Serializable {
+
+    /**
+     * Creates a new instance of MbSLogin
+     */
+    private String correoElectronico;
+    private String contrasenia;
+    
+    private Session session;
+    private Transaction transaccion;
+    
+    public MbSLogin() 
+    {
+    }
+    
+    public String login()
+    {
+        this.session=null;
+        this.transaccion=null;
+        
+        try
+        {
+            DaoTUsuario daoTUsuario=new DaoTUsuario();
+            
+            this.session=HibernateUtil.getSessionFactory().openSession();
+            this.transaccion=this.session.beginTransaction();
+            
+            Tusuario tUsuario=daoTUsuario.getByCorreoElectronico(this.session, this.correoElectronico);
+            
+            if(tUsuario!=null)
+            {
+                if(tUsuario.getContrasenia().equals(Encrypt.sha512(this.contrasenia)))
+                {
+                    return "/usuario/vertodo";
+                }
+            }
+            
+            this.transaccion.commit();
+            
+            this.correoElectronico=null;
+            this.contrasenia=null;
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error de acceso:", "Usuario o contraseña incorrecto"));
+            
+            return "/index";
+            
+        }
+        catch(Exception ex)
+        {
+            if(this.transaccion!=null)
+            {
+                this.transaccion.rollback();
+            }
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error fatal:", "Por favor contacte con su administrador "+ex.getMessage()));
+            
+            return null;
+        }
+        finally
+        {
+            if(this.session!=null)
+            {
+                this.session.close();
+            }
+        }
+    }
+    
+    public String cerrarSesion()
+    {
+        this.correoElectronico=null;
+        this.contrasenia=null;
+        
+        return "/index";
+    }
+    
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+}
