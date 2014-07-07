1c635095503b9dcf0809143e29455f5b5376a07e
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2014-07-07 14:29:45 -0500

    EDITAR DATOS EN LISTA DE USUARIOS

--- APPAGENDAJAVA/src/java/Dao/DaoTUsuario.java
+++ APPAGENDAJAVA/src/java/Dao/DaoTUsuario.java
@@ -37,7 +37,7 @@ public class DaoTUsuario implements InterfaceTUsuario{
 
     @Override
     public Tusuario getByCodigoUsuario(Session session, String codigoUsuario) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (Tusuario) session.get(Tusuario.class, codigoUsuario);
     }
     
     @Override
